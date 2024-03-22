09d64b4b0903356e508c22e2e4912051ac316ef2
Victor Queiroz <victorqueiroz.ti@gmail.com>
2024-03-22 00:24:46 -0300

    created the lib folder to organize dependencies

--- src/br/com/caloteiros/dao/CaloteiroDAO.java
+++ src/br/com/caloteiros/dao/CaloteiroDAO.java
@@ -15,25 +15,25 @@ import br.com.caloteiros.model.Caloteiro;
 public class CaloteiroDAO {
 
 	private Connection connection;
-	
-	
+
+
 	public CaloteiroDAO() {
 		this.connection = new ConnectionFactory().getConnection();
 	}
-	
-	
+
+
 	public void add(Caloteiro caloteiro) {
 		String stmt = "insert into caloteiro " +
 				"(name, email, debt, debtDate) " +
 				"values (?,?,?,?)";
 		try {
 			PreparedStatement pstmt = this.connection.prepareStatement(stmt);
-			
+
 			pstmt.setString(1, caloteiro.getName());
 			pstmt.setString(2, caloteiro.getEmail());
 			pstmt.setInt(3, caloteiro.getDebt());
 			pstmt.setDate(4, new Date(caloteiro.getDebtDate().getTimeInMillis()));
-			
+
 			pstmt.execute();
 			pstmt.close();
 		} catch (SQLException e) {
@@ -41,16 +41,16 @@ public class CaloteiroDAO {
 		}
 	}
 
-	
+
 	public List<Caloteiro> getList() {
 		try {
 			PreparedStatement stmt = this.connection.
 					prepareStatement("select * from caloteiro;");
-			
-			List<Caloteiro> caloteiros = new ArrayList<Caloteiro>();
+
+			List<Caloteiro> caloteiros = new ArrayList<>();
 			Caloteiro caloteiro = null;
 			ResultSet rs = stmt.executeQuery();
-			
+
 			while(rs.next()) {
 				Long idCaloteiro = (long) rs.getInt("id");
 				String name = rs.getString("name");
@@ -58,74 +58,74 @@ public class CaloteiroDAO {
 				int debt = rs.getInt("debt");
 				Calendar debtDate = Calendar.getInstance();
 				debtDate.setTime(rs.getDate("debtDate"));
-				
+
 				caloteiro = new Caloteiro();
-				
+
 				caloteiro.setId(idCaloteiro);
 				caloteiro.setName(name);
 				caloteiro.setEmail(email);
 				caloteiro.setDebt(new Integer(debt));
 				caloteiro.setDebtDate(debtDate);
-				
+
 				caloteiros.add(caloteiro);
 			}
-			
+
 			rs.close();
 			stmt.close();
 			return caloteiros;
-			
+
 		} catch(SQLException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
-	
+
 	public void update(Caloteiro caloteiro) {
 		String sql = "update caloteiro set name=?, email=?,"+
 				"debt=?, debtDate=? where id=?";
-		
+
 		try {
 			PreparedStatement stmt = this.connection.prepareStatement(sql);
-			
+
 			stmt.setString(1, caloteiro.getName());
 			stmt.setString(2, caloteiro.getEmail());
 			stmt.setInt(3, caloteiro.getDebt());
 			stmt.setDate(4, new Date(caloteiro.getDebtDate().getTimeInMillis()));
 			stmt.setLong(5, caloteiro.getId());
-			
+
 			stmt.execute();
 			stmt.close();
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
-	
+
+
 	public void delete(Caloteiro caloteiro) {
 		String sql = "delete from caloteiro where id=?";
-		
+
 		try {
 			PreparedStatement stmt = this.connection.prepareStatement(sql);
-			
+
 			stmt.setLong(1, caloteiro.getId());
-			
+
 			stmt.execute();
 			stmt.close();
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
-	
+
+
 	public Caloteiro getCaloteiro(Long id) {
 		try {
 			PreparedStatement stmt = this.connection.
 					prepareStatement("select * from caloteiro where id=?");
 			stmt.setLong(1, id);
-			
+
 			Caloteiro caloteiro = null;
 			ResultSet rs = stmt.executeQuery();
-			
+
 			if(rs.next()) {
 				Long idCaloteiro = rs.getLong("id");
 				String name = rs.getString("name");
@@ -133,25 +133,25 @@ public class CaloteiroDAO {
 				int debt = rs.getInt("debt");
 				Calendar debtDate = Calendar.getInstance();
 				debtDate.setTime(rs.getDate("debtDate"));
-				
+
 				caloteiro = new Caloteiro();
-				
+
 				caloteiro.setId(idCaloteiro);
 				caloteiro.setName(name);
 				caloteiro.setEmail(email);
 				caloteiro.setDebt(new Integer(debt));
 				caloteiro.setDebtDate(debtDate);
 			}
-			
+
 			rs.close();
 			stmt.close();
 			return caloteiro;
-			
+
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 }
 
 
--- src/br/com/caloteiros/model/Caloteiro.java
+++ src/br/com/caloteiros/model/Caloteiro.java
@@ -3,52 +3,52 @@ package br.com.caloteiros.model;
 import java.util.Calendar;
 
 public class Caloteiro {
-	
+
 	private Long id;
 	private String name;
 	private String email;
 	private Integer debt;
 	private Calendar debtDate;
-	
-	
+
+
 	public Long getId() {
 		return id;
 	}
-	
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public String getEmail() {
 		return email;
 	}
-	
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 	public Integer getDebt() {
 		return debt;
 	}
-	
+
 	public void setDebt(Integer debt) {
 		this.debt = debt;
 	}
-	
+
 	public Calendar getDebtDate() {
 		return debtDate;
 	}
-	
+
 	public void setDebtDate(Calendar debtDate) {
 		this.debtDate = debtDate;
 	}
-	
+
 }
